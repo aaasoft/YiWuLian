6615943b41e5e953db7b948b2ba5865dcb57881b
张鹏 <scbeta@qq.com>
2026-02-27 10:41:47 +0800

    首页增加每秒自动刷新功能

--- src/YiWuLian.Server/Components/Pages/Home.razor.cs
+++ src/YiWuLian.Server/Components/Pages/Home.razor.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Components;
 using Quick.EntityFrameworkCore.Plus;
-using YiWuLian.Server.Core.Interfaces.Device;
 
 namespace YiWuLian.Server.Components.Pages;
 
-public partial class Home : ComponentBase
+public partial class Home : ComponentBase,IDisposable
 {
     private int GetDevicesCount() => ConfigDbContext.CacheContext.Query<Models.YIS_Device>().Length;
 
     private int GetConnectionCount() => ConfigDbContext.CacheContext.Query<Models.YIS_Device>(t => t.ConnectionInfo.IsConnected).Length;
+
+    private Timer timer;
+
+    protected override void OnInitialized()
+    {
+        timer = new Timer(refresh, null, 1000, 1000);
+    }
+    
+    private void refresh(object _)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+        timer = null;
+    }
 }
